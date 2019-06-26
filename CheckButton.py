c457e22108678fec474b9bc7bb47995702fb119f
3194libin <15764747737@qq.com>
2019-06-26 17:45:10 +0800

    脚本语言

--- CheckButton.py
+++ CheckButton.py
@@ -1,13 +1,17 @@
 from tkinter import *
 root = Tk()
 
-GIRLS=['西施','貂蝉','王昭君','杨玉环']
+LANGS = [
+    ('Python',1),
+    ('Perl',2),
+    ('Ruby',3),
+    ('Lua',4)]
 
-v=[]
+v = IntVar()
+v.set(1)
 
-for girl in GIRLS:
-    v.append(IntVar())
-    b = Checkbutton(root,text=girl,variable = v[-1])
-    b.pack(anchor = W )
+for lang,num in LANGS:
+    b = Radiobutton(root,text=lang,variable=v,value=num)
+    b.pack(anchor=W)
 
 mainloop()
